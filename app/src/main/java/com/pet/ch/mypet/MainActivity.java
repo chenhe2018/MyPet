3499081e483f18ec816e879a3e6c93d6f2482dc5
secondwatch <watch_ch@163.com>
2019-01-07 17:36:00 +0800

    gradle

--- app/src/main/java/com/pet/ch/mypet/MainActivity.java
+++ app/src/main/java/com/pet/ch/mypet/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
