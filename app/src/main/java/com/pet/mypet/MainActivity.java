6baf7d8c463bd6bd3ca2bc8bb90f02d6c11a40ac
secondwatch <watch_ch@163.com>
2019-01-09 14:05:14 +0800

    permission

---
+++ app/src/main/java/com/pet/mypet/MainActivity.java
@@ -0,0 +1,86 @@
+package com.pet.mypet;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import permission.FloatWindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btn_start = (Button) findViewById(R.id.main_start_service);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "service已开启", Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this, PetService.class);
+//                startService(intent);
+                FloatWindowManager.getInstance().applyOrShowFloatWindow(MainActivity.this);
+            }
+        });
+
+        Button btn_stop = (Button) findViewById(R.id.main_stop_service);
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "service已关闭", Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent();
+//                intent.setAction("com.pet.mypet.PetService");
+//                stopService(intent);
+                FloatWindowManager.getInstance().dismissWindow();
+            }
+        });
+
+        Button btn_permission = (Button) findViewById(R.id.permission);
+        btn_permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "获取权限", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
